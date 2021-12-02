c7a8e7f6919edbc608fcacb8c763cafd922ab293
luizbaruer <luizbarueri@gmail.com>
2021-12-02 18:55:16 -0300

    alterando a pagina index.html do angular para meu site

--- AdoteMeusGatos/src/app/app.component.spec.ts
+++ AdoteMeusGatos/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AdoteMeusGatos app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('site AdoteMeusGatos está rodando!');
   });
 });
